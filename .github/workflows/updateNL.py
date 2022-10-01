6ac2ef1c5e5e6c8652ce0cd49bc91cc6e29861ea
Chris John <Chris.John15@outlook.com>
2022-10-01 14:03:33 -0400

    testing gh actions here

---
+++ .github/workflows/updateNL.py
@@ -0,0 +1,13 @@
+from bs4 import BeautifulSoup as bs
+import os
+import re
+
+base = os.path.dirname(os.path.abspath(__file__))
+
+html = open(os.path.join(base, './includes/header.html'))
+
+soup = bs(html, 'html.parser')
+
+old_text = soup.find("a", {"id":"newsletter-link"})
+
+new_text = old_text.find(text=re.compile('August2022.pdf')).replace_with('October2022.pdf')
